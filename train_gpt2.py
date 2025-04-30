9b41f1e75726ea9e5ef1c3bd8b6c2c980d3d3569
Kyle Ng <kgng@usc.edu>
2025-04-30 05:10:15 -0700

    fix architecture to work with existing gpt2 hf implementation

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
-from torch.functional import functional as F
+from torch.nn import functional as F
 """
 Differences from Transformer Paper
 Layer norms go before MLPs and Layer norm at the end
@@ -12,11 +12,11 @@ Encoder only, no cross attention (special attention used in encoders)
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256  # Max Context length
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384  # Hidden Size
+    block_size: int = 1024  # Max Context length
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768  # Hidden Size
 
 
 class CausalSelfAttention(nn.Module):
@@ -32,14 +32,14 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
 
         # Output project (Mixing of all of the heads)
-        self.c_project = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
 
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.n_hs = config.n_embd // config.n_head
 
         # Create a mask for the autoregressive attention, use .view(1,1,T,T) to broadcast to #batches/#heads later
-        self.register("bias", torch.trill(torch.ones(config.block_size, config.block_size).view(
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size).view(
             1, 1, config.block_size, config.block_size)))
 
     def forward(self, x: torch.tensor):
@@ -93,7 +93,7 @@ class CausalSelfAttention(nn.Module):
         y = attn @ v
 
         # Step 4) Final mixing
-        y = self.c_project(y)
+        y = self.c_proj(y)
         return y
 
 
@@ -166,3 +166,53 @@ class GPT(nn.Module):
 
         # Language head at the end
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    # Initialization code from Karpathy repo to test validity of our version
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
